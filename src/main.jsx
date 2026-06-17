8d5e3a85eeb692357bab4cef1cc1032bf2a5cd61
Developer <dev@example.com>
2026-06-17 20:16:48 +0530

    bug fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
+import axios from 'axios'
+import API_BASE_URL from './config.js'
 import './index.css'
 import App from './App.jsx'
+
+// Set the base URL for all Axios requests
+axios.defaults.baseURL = API_BASE_URL;
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
